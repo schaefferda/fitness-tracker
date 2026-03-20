e48d13459028e21da7b31560fbe728e988f7f660
Daniel Schaeffer <daniel.schaeffer@protonmail.com>
2026-03-19 22:12:06 -0600

    Add login/logout functionality

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,7 @@ urlpatterns = [
     path('add/', views.add_workout, name='add_workout'),
     path('edit/<int:pk>/', views.edit_workout, name='edit_workout'),
     path('delete/<int:pk>/', views.delete_workout, name='delete_workout'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='workouts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
