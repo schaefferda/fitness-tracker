dde74dd8b167c9d08f00dd3f2dd38e18703b8970
Daniel Schaeffer <daniel.schaeffer@protonmail.com>
2026-03-24 07:03:52 -0600

    Automate User Profile creation
    Added "signals" to automate Profile creation. Prior to building any User Settings page we need to push these changes and apply them on the production server. Since signals only fire when a new action happens, we need to perform an existing user catch-up since we already have users on the live app. This can be done with the following commands
    
    ```
    cd /var/www/fitness-tracker
    python manage.py shell
    
    from django.contrib.auth.models import User
    from workouts.models import Profile
    
    for user in User.objects.all():
        Profile.objects.get_or_create(user=user)
    
    ```

--- workouts/apps.py
+++ workouts/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class WorkoutsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'workouts'
+
+    def ready(self):
+        import workouts.signals
